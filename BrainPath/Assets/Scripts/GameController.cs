8c1681fa75e044856db548f48c5c245b6e11975f
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-04 13:42:45 -0400

    Add singleton game controller

---
+++ BrainPath/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    //Make sure GameController is singleton and easily accessible
+    private static GameController instance;
+    public GameController getInstance() { if (instance) { return instance; } else { return new GameController(); } }
+    void Awake()
+    {
+        instance = this;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
