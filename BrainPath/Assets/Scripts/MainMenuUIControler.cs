052e50d68bf8df0f15238c59f096a1380bf8b045
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-09 09:25:15 -0400

    Create main menu with a spinning brain on the background

---
+++ BrainPath/Assets/Scripts/MainMenuUIControler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuUIControler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("TutorialButton").GetComponent<Button>().onClick.AddListener(OnClickTutorial);
+        GameObject.Find("ExitButton").GetComponent<Button>().onClick.AddListener(OnClickExit);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClickTutorial()
+    {
+        Application.LoadLevel("Tutorial");
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ BrainPath/Assets/Scripts/MenuCameraMover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCameraMover : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector3 relCameraPos;       // The relative position of the camera from the player.
+    private float relCameraPosMag;      // The distance of the camera from the player.
+    private Vector3 newPos;             // The position the camera is trying to reach.
+    private Vector3 offset;
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position;
+
+        relCameraPos = transform.position - FocusPoint.position;
+        relCameraPosMag = relCameraPos.magnitude - 0.5f;
+
+    }
+    public float turnSpeed = 4.0f;
+    public Transform FocusPoint;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = new Vector3(FocusPoint.position.x, FocusPoint.position.y, FocusPoint.position.z) + offset;
+        transform.position = pos;
+        offset = Quaternion.AngleAxis(turnSpeed, Vector3.up) * offset;
+        //offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.left) * offset;
+        transform.position = FocusPoint.position + offset;
+        transform.LookAt(FocusPoint.position);
+    }
+}
