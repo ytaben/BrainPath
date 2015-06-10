642f3743b50fc54ef54454c4729d9efe1787333b
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-10 18:36:57 -0400

    Many general changes to the scene

---
+++ BrainPath/Assets/MaterialController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialController : MonoBehaviour {
+    public Material undiscoveredMaterial, currentMaterial, outboundMaterial;
+    // Use this for initialization
+    private static MaterialController instance;
+    public static MaterialController getInstance()
+    {
+        return instance ? instance : new MaterialController();
+    }
+    void Awake()
+    {
+        instance = this;
+    }
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
