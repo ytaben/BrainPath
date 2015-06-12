d326e4b30fda89eef0178be6d5e0c48f58199c56
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-12 16:04:03 -0400

    Add Brain animation for future use

--- BrainPath/Assets/Scripts/VisualCortexButton.cs
+++ BrainPath/Assets/Scripts/VisualCortexButton.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class VisualCortexButton : MonoBehaviour {
-
+    bool anim;
 	// Use this for initialization
 	void Start () {
-	
+        anim = false;
+        GetComponent<Button>().onClick.AddListener(OnClick);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    public void OnClick() {
+        //Animator brain = GameObject.Find("Brain").GetComponent<Animator>();
+        //anim = !anim;
+        //brain.SetBool("IsSeparated", anim);
+    }
 }
