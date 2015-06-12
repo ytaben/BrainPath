916cf459e83ba970a90f31926fc624db996cccdb
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-12 12:14:40 -0400

    Implement changing colors of brain parts based on status

--- BrainPath/Assets/Scripts/BrainNode.cs
+++ BrainPath/Assets/Scripts/BrainNode.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 public class BrainNode : MonoBehaviour
 {
     public bool isActive; //Determine whether this node is currently selected
+    public bool isExplored;
+    public bool isNew;
 
     public GameObject[] outboundNodes; //List of nodes that are connected to this one
+    public GameObject[] brainParts; //List of brain parts that correspond to this node
 
     // private CanvasGroup canvasGroup; //Useful to control alpha of the element
     private Button button;
     private GameController gameController; //Reference to gameController (singleton)
+    private MaterialController materialController;
     // Use this for initialization
     void Start()
     {
         //canvasGroup = GetComponent<CanvasGroup>(); //Initialize member variables
         gameController = GameController.getInstance();
+        materialController = MaterialController.getInstance();
         button = GetComponent<Button>();
         button.onClick.AddListener(OnClickBrainNode); //Add an onClick listener 
     }
@@ -23,18 +28,41 @@ public class BrainNode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ColorBlock buttonColors;
+        if (!isExplored)
+        {
+            buttonColors = button.colors;
+            buttonColors.normalColor = Color.green;
+            button.colors = buttonColors;
+            SetMaterial(materialController.undiscoveredMaterial);
+            //canvasGroup.alpha = 1;
+        }
+        if (isNew)
+        {
+            SetMaterial(materialController.outboundMaterial);
+        }
         //If this node is active, pulse by changing alpha
         if (isActive)
         {
-            button.colors.normalColor = Color.green;
+            buttonColors = button.colors;
+            buttonColors.normalColor = Color.green;
+            button.colors = buttonColors;
+            SetMaterial(materialController.currentMaterial);
             //canvasGroup.alpha = Mathf.PingPong(Time.time, 1);
 
         }
-        else
-            button.colors.normalColor = Color.black;
-            //canvasGroup.alpha = 1;
+
     }
 
+    private void SetMaterial(Material material)
+    {
+        foreach (GameObject brainPart in brainParts)
+        {
+            Material[] mats = brainPart.GetComponent<Renderer>().materials;
+            mats[0] = material;
+            brainPart.GetComponent<Renderer>().materials = mats;
+        }
+    }
     //On Click - notify gameController using the Transition method
     void OnClickBrainNode()
     {
---
+++ BrainPath/Assets/Scripts/BrainNode3D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrainNode3D : MonoBehaviour {
+
+    MaterialController materialController;
+	// Use this for initialization
+	void Start () {
+        materialController = MaterialController.getInstance();
+	}
+    //void OnMouseOver()
+    //{
+    //    Material[] mats = GetComponent<Renderer>().materials;
+    //    mats[0] = materialController.currentMaterial;
+    //    GetComponent<Renderer>().materials = mats;
+    //}
+    //void OnMouseExit()
+    //{
+    //    Material[] mats = GetComponent<Renderer>().materials;
+    //    mats[0] = materialController.undiscoveredMaterial   ;
+    //    GetComponent<Renderer>().materials = mats;
+    //}
+
+
+}
