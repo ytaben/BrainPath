18494078b3d32d7629072efdd54d95b0b82fc538
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-22 13:31:30 -0400

    Add TutorialController to run the tutorial of a level

--- BrainPath/Assets/Scripts/TutorialController.cs
+++ BrainPath/Assets/Scripts/TutorialController.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TutorialController : MonoBehaviour {
+public class TutorialController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private static TutorialController instance;
+
+    public static TutorialController getInstance() { return instance; }
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public int totalStages;
+    public int currentStage = 0;
+    public GameObject[] helpScreens;
+    // Use this for initialization
+    void Start()
+    {
+        helpScreens[currentStage].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void IncrementStage()
+    {
+        helpScreens[currentStage].SetActive(false);
+        currentStage++;
+        if (currentStage <= totalStages)
+        {
+            helpScreens[currentStage].SetActive(true);
+        }
+    }
 }
