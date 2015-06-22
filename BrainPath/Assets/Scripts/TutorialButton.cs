aadb4a2af770a5a77f8e2acf08d71d94a946484d
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-22 13:35:21 -0400

    Implement TutorialButton class

--- BrainPath/Assets/Scripts/TutorialButton.cs
+++ BrainPath/Assets/Scripts/TutorialButton.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TutorialButton : MonoBehaviour {
-
+    public int correctStage;
+    private TutorialController tutorialController;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        GetComponent<Button>().onClick.AddListener(OnClick);
+        tutorialController = TutorialController.getInstance();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void OnClick() {
+        if (tutorialController.currentStage == correctStage) tutorialController.IncrementStage();
+    }
 }
