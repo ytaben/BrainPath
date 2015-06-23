181ed0876b20263679d2ce894ebf0fc8aab0d6c4
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-23 13:51:30 -0400

    Add LevelLoader script for buttons that load levels

---
+++ BrainPath/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public string levelName;
+	// Use this for initialization
+	void Start () {
+        //Attach onClick to the button
+        Button button = GetComponent<Button>();
+        if (button) { button.onClick.AddListener(OnClick); }
+        else Debug.Log("LevelLoader script must be attached to a button!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnClick()
+    {
+        Application.LoadLevel(levelName);
+    }
+}
