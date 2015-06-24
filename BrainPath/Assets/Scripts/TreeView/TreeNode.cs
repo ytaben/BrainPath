4aaef329f11f9e78fb7266e52dd62620ae8b3d53
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-24 12:20:41 -0400

    Add TreeNode class for implementing TreeView UI

---
+++ BrainPath/Assets/Scripts/TreeView/TreeNode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeNode : MonoBehaviour {
+
+    public GameObject[] children;
+    public GameObject nextNode;
+
+    private bool isExpanded;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnClick()
+    {
+
+    }
+}
