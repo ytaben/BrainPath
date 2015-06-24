0bbb8250efb98f8ec860d22d4e2aec6d6163fb49
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-24 17:30:06 -0400

    WIP. Very messy development of TreeView
    Allow trees to expand and collapse

--- BrainPath/Assets/Scripts/TreeView/TreeNode.cs
+++ BrainPath/Assets/Scripts/TreeView/TreeNode.cs
@@ -15,18 +15,35 @@ public class TreeNode : MonoBehaviour {
     private RectTransform rectTransform;
     private RectTransform iconTransform;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject tree;
+
+    void Awake()
+    {
         rectTransform = GetComponent<RectTransform>();
-        offset = rectTransform.sizeDelta.y;
 
         iconTransform = (RectTransform)rectTransform.FindChild("ArrowIcon");
+        offset = rectTransform.sizeDelta.y;
+    }
+	// Use this for initialization
+	void Start () {
+        
+        
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         GetComponent<Button>().onClick.AddListener(OnClick);
 	}
+    public void DeactivateChildren()
+    {
+        foreach (GameObject child in children)
+        {
+            child.GetComponent<TreeNode>().DeactivateChildren();
+            
+        }
+        gameObject.SetActive(false);
+    }
 
     void OnClick()
     {
@@ -35,24 +52,30 @@ public class TreeNode : MonoBehaviour {
             isExpanded = false;
             foreach (GameObject child in children)
             {
-                child.SetActive(false);
+                child.GetComponent<TreeNode>().DeactivateChildren();
             }
+            
         }
         else
         {
             isExpanded = true;
-            float verticalOffset = (float)(offset * 1.5);
             foreach (GameObject child in children)
             {
                 child.SetActive(true);
-                Vector2 position = child.GetComponent<RectTransform>().position;
-                position.x = rectTransform.position.x + horizontalOffset;
-                position.y = rectTransform.position.y - verticalOffset;
-                child.GetComponent<RectTransform>().position = position;
-                verticalOffset += offset;
             }
+            //float verticalOffset = (float)(offset * 1.5);
+            //foreach (GameObject child in children)
+            //{
+            //    child.SetActive(true);
+            //    Vector2 position = child.GetComponent<RectTransform>().position;
+            //    position.x = rectTransform.position.x + horizontalOffset;
+            //    position.y = rectTransform.position.y - verticalOffset;
+            //    child.GetComponent<RectTransform>().position = position;
+            //    verticalOffset += offset;
+            //}
         }
         SetIconRotation();
+        tree.GetComponent<TreeView>().RefreshTree();
     }
 
     public void SetIconRotation()
@@ -65,5 +88,24 @@ public class TreeNode : MonoBehaviour {
         {
             iconTransform.rotation = Quaternion.Euler(0, 0, 90);
         }
+    }
+    
+    public float DrawChildren()
+    {
+        float verticalOffset = (float)(offset * 1.5);
+        if (true) {//isExpanded) { 
+        
+        foreach (GameObject child in children)
+        {
+            Vector2 position = GetComponent<RectTransform>().position;
+            position.x = rectTransform.position.x + horizontalOffset;
+            position.y = rectTransform.position.y - verticalOffset;
+                child.GetComponent<RectTransform>().position = position;
+                verticalOffset += child.GetComponent<TreeNode>().DrawChildren();
+         }
+            
+        }
+        return verticalOffset;
+        return 0; //remove this
     }
 }
---
+++ BrainPath/Assets/Scripts/TreeView/TreeView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeView : MonoBehaviour {
+
+    public GameObject[] nodes; //Highest level nodes
+
+    public float initialHorizontal, initialVertical;
+
+	// Use this for initialization
+	void Start () {
+        RefreshTree();
+	}
+
+    public void RefreshTree()
+    {
+        float horizontalOffset = initialHorizontal;
+        float verticalOffset = initialVertical;
+        foreach (GameObject node in nodes)
+        {
+            Vector2 position = GetComponent<RectTransform>().position;
+            position.x += horizontalOffset;
+            position.y -= verticalOffset;
+            node.GetComponent<RectTransform>().position = position;
+            verticalOffset += node.GetComponent<TreeNode>().DrawChildren();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
