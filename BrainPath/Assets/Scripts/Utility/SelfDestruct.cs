c0bdd6acea000398f8d160fbab3ebc088442550f
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-06-28 14:29:49 -0400

    Add a loading screen to the tutorial sceene

---
+++ BrainPath/Assets/Scripts/Utility/SelfDestruct.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour {
+
+    public float time;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
