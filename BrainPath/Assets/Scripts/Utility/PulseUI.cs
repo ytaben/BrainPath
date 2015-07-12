ff07294db892438765e8210a2d45063a209efaa4
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-12 13:17:13 -0400

    Fix pulse UI bugs
    -Restore alpha when pulsing finishes
    -Actually pulse
    -Take timeout into account

--- BrainPath/Assets/Scripts/Utility/PulseUI.cs
+++ BrainPath/Assets/Scripts/Utility/PulseUI.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 /// Make UI element pulse by changing its alpha
 /// This script is active as long as the object containing it is active
 /// </summary>
-public class PulseUI : MonoBehaviour {
+public class PulseUI : MonoBehaviour
+{
 
     public GameObject[] elements;
 
@@ -16,8 +17,9 @@ public class PulseUI : MonoBehaviour {
     private float stopTime; //Time when we must stop pulsing
     private bool isPulsing;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         //Canvas group controlls alpha, so we need it attached to every pulsing element
         foreach (GameObject element in elements)
         {
@@ -26,21 +28,25 @@ public class PulseUI : MonoBehaviour {
                 element.AddComponent<CanvasGroup>();
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (timeout <= 0 && Time.time > stopTime) isPulsing = false; //Stop if we have a timeout and it has passed
+    }
 
-        if (isPulsing)
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeout > 0 && Time.time > stopTime) isPulsing = false; //Stop if we have a timeout and it has passed
+
+
+        foreach (GameObject element in elements)
         {
-            foreach (GameObject element in elements)
+            CanvasGroup cg = element.GetComponent<CanvasGroup>();
+            if (isPulsing)
             {
-                CanvasGroup cg = element.GetComponent<CanvasGroup>();
-                cg.alpha = Mathf.PingPong(cg.alpha, Time.time);
+                cg.alpha = Mathf.PingPong(Time.time, 1);
             }
+            else cg.alpha = 1;
         }
-	}
+
+    }
 
 
     //This may be somewhat redundant, but we can make it just in case
