256e226389ec6391292efda26408c32f18cec389
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-12 13:02:25 -0400

    Create list of objects to be enabled when the game starts
    There is no longer a need to turn help screens and loading screens on and off

---
+++ BrainPath/Assets/Scripts/Utility/EnableOnWake.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableOnWake : MonoBehaviour {
+
+    public GameObject[] gameObjects;
+
+	// Use this for initialization
+	void Start () {
+	    foreach (GameObject gameObject in gameObjects)
+        {
+            gameObject.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
