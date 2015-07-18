2e04d2469255eef81616b8cd3665c464bdae7118
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-18 12:25:45 -0400

    Spawn a label for every 3D part of the brain
    The label is displayed at the mouse cursor while hovering over.
    Label text contains the name of the node as specified in BrainNode
    Label disappears once mouse exists.

--- BrainPath/Assets/Scripts/BrainNode3D.cs
+++ BrainPath/Assets/Scripts/BrainNode3D.cs
@@ -5,6 +5,15 @@ using UnityEngine.UI;
 public class BrainNode3D : MonoBehaviour {
 
     public BrainNode brainNode; //Reference to the actual brain node that controlls this script
+    public GameObject labelPrefab; //Prefab for the animated label
+
+    private GameObject label; //Track if we already have a label created
+    Text labelText;
+    UITracker labelPositioner;
+    LabelAnimator labelAnimator;
+
+    private bool needLabelDespawn;
+    private bool needLabelSpawn;
 
     MaterialController materialController;
 	// Use this for initialization
@@ -12,11 +21,20 @@ public class BrainNode3D : MonoBehaviour {
         materialController = MaterialController.getInstance();
 	}
 
+    void Update()
+    {
+        if (needLabelDespawn && !labelAnimator.Busy()) { labelAnimator.Collapse(); needLabelDespawn = false; }
+        if (needLabelSpawn && !labelAnimator.Busy()) { labelAnimator.Expand(); needLabelSpawn = false; }
+    }
+
     void OnMouseOver()
     {
         if (brainNode &&  brainNode.isExplored)
         {
             brainNode.OnPointerEnter();
+
+            SpawnLabel();
+            needLabelDespawn = false;
         }
     }
 
@@ -25,6 +43,8 @@ public class BrainNode3D : MonoBehaviour {
         if (brainNode && brainNode.isExplored)
         {
             brainNode.OnPointerExit();
+            DespawnLabel();
+            needLabelSpawn = false;
         }
     }
 
@@ -33,6 +53,34 @@ public class BrainNode3D : MonoBehaviour {
         if (brainNode &&  brainNode.isExplored)
         {
             brainNode.OnClickBrainNode();
+        }
+    }
+
+    private void SpawnLabel()
+    {
+        if (!label)
+        {
+            label = Instantiate(labelPrefab) as GameObject;
+            label.transform.SetParent(GameObject.Find("Canvas").transform);
+        }
+        if (!labelText) labelText = label.GetComponentInChildren<Text>();
+        labelText.text = brainNode.name;
+
+        if (!labelPositioner) labelPositioner = label.GetComponent<UITracker>();
+        labelPositioner.SetPosition(Input.mousePosition);
+
+        if (!labelAnimator) labelAnimator = label.GetComponent<LabelAnimator>();
+        labelAnimator.labelText = brainNode.name;
+        if (labelAnimator.Busy()) needLabelSpawn = true;
+        else labelAnimator.Expand();
+    }
+
+    private void DespawnLabel()
+    {
+        if (label)
+        {
+            if (labelAnimator.Busy()) needLabelDespawn = true;
+            else labelAnimator.Collapse();
         }
     }
 
