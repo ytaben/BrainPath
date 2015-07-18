2443adb5181954ef90a6ed3dc8e47fb81ce20621
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-18 11:40:15 -0400

    Disable ongoing tracking for label and enable setting fixed position

--- BrainPath/Assets/Scripts/UITracker.cs
+++ BrainPath/Assets/Scripts/UITracker.cs
@@ -6,15 +6,13 @@ public class UITracker : MonoBehaviour {
     public GameObject tracked;
 
     private RectTransform rect;
-    private Vector3 offset;
 	// Use this for initialization
 	void Start () {
         rect = GetComponent<RectTransform>();
-        offset = rect.position - tracked.transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        rect.position = new Vector3(tracked.transform.position.x, tracked.transform.position.y, tracked.transform.position.z);
-	}
+	public void SetPosition(Vector3 position)
+    {
+        rect.transform.position = position;
+    }
 }
