93753979bec854a7eef05d07fd43593985768b22
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-20 09:27:33 -0400

    Implement modal panel script

--- BrainPath/Assets/Scripts/ModalPanel.cs
+++ BrainPath/Assets/Scripts/ModalPanel.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class ModalPanel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Text titleText;
+    public Text messageText;
+    public Button okButton;
+    public Button cancelButton;
+    public GameObject modalPanelObject;
+
+
+    //Make modal panel singleton
+    private static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+        }
+        else Debug.Log("Modal Panel Instance Not found");
+
+        return modalPanel;
+    }
+
+    /// <summary>
+    /// Prompt a modal panel with given title, messages and events
+    /// </summary>
+    /// <param name="title">Title to be displayed</param>
+    /// <param name="message">Message to be displayed</param>
+    /// <param name="okAction">Action to perform when Ok is clicked</param>
+    /// <param name="cancelAction">Action to perform when Cancel is clicked</param>
+    public void Prompt(string title, string message, UnityAction okAction, UnityAction cancelAction)
+    {
+        titleText.text = title;
+        messageText.text = message;
+
+        okButton.onClick.RemoveAllListeners();
+        okButton.onClick.AddListener(okAction);
+        okButton.onClick.AddListener(() => modalPanelObject.SetActive(false));
+
+        cancelButton.onClick.RemoveAllListeners();
+        cancelButton.onClick.AddListener(cancelAction);
+        cancelButton.onClick.AddListener(() => modalPanelObject.SetActive(false));
+
+        modalPanelObject.SetActive(true);
+    }
 }
