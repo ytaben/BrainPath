86f650b44895f859ade4c8e5d78dda4c2fb858d1
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-26 21:13:18 -0400

    Add PlaySoundOnEnable utility script

---
+++ BrainPath/Assets/Scripts/SoundUtility/PlaySoundOnEnable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnEnable : MonoBehaviour {
+
+    public AudioClip sound;
+    AudioSource mainAudioSource;
+	// Use this for initialization
+	void Start () {
+        mainAudioSource = Camera.main.GetComponent<AudioSource>();
+        if (!mainAudioSource) Debug.Log("Main camera doesn't have Audio Source!");
+	}
+	
+	void OnEnable()
+    {
+        mainAudioSource.PlayOneShot(sound);
+    }
+}
