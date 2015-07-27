8440734e6daef0ad94e6b11e30016a240e5988bd
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-26 22:15:46 -0400

    Add playOnClickSound script

---
+++ BrainPath/Assets/Scripts/SoundUtility/PlaySoundOnClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class PlaySoundOnClick : MonoBehaviour {
+
+    private SoundManager soundManager;
+    private AudioSource mainAudioSource;
+    private Button button;
+    // Use this for initialization
+    void Start()
+    {
+        soundManager = SoundManager.Instance();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+        mainAudioSource = Camera.main.GetComponent<AudioSource>();
+    }
+
+    void OnClick()
+    {
+        mainAudioSource.PlayOneShot(soundManager.mouseOverSound);
+    }
+}
