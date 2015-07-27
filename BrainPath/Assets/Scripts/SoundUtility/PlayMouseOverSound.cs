85a3d00f4526b213a3151659cad9c0f569887960
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-26 22:13:25 -0400

    Add utility PlayMouseOver sound script

---
+++ BrainPath/Assets/Scripts/SoundUtility/PlayMouseOverSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+[RequireComponent(typeof(EventTrigger))]
+public class PlayMouseOverSound : MonoBehaviour, IPointerEnterHandler {
+    private SoundManager soundManager;
+    private AudioSource mainAudioSource;
+    private Button button;
+    // Use this for initialization
+    void Start()
+    {
+        soundManager = SoundManager.Instance();
+        button = GetComponent<Button>();
+        mainAudioSource = Camera.main.GetComponent<AudioSource>();
+
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mainAudioSource.PlayOneShot(soundManager.mouseOverSound);
+    }
+}
