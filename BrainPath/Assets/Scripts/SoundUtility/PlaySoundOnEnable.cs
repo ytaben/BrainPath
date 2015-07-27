f426e857fb3f8b3a966623b35851bb2fa37289d8
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-07-26 21:19:20 -0400

    Fix a bug with null AudioSource in PlayOnEnable
    The bug is caused by ModalPanel being disabled from the start, so Start() is never called

--- BrainPath/Assets/Scripts/SoundUtility/PlaySoundOnEnable.cs
+++ BrainPath/Assets/Scripts/SoundUtility/PlaySoundOnEnable.cs
@@ -6,13 +6,13 @@ public class PlaySoundOnEnable : MonoBehaviour {
     public AudioClip sound;
     AudioSource mainAudioSource;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         mainAudioSource = Camera.main.GetComponent<AudioSource>();
         if (!mainAudioSource) Debug.Log("Main camera doesn't have Audio Source!");
 	}
 	
 	void OnEnable()
-    {
+    {   if (!sound) Debug.Log("bad");
         mainAudioSource.PlayOneShot(sound);
     }
 }
