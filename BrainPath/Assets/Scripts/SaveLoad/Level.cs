b61cd63c4ee0b7b55761312c07ed1db51331154d
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-03 17:10:08 -0400

    Start of work on load/save mechanism!
    Add level struct and manager class

---
+++ BrainPath/Assets/Scripts/SaveLoad/Level.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour {
+    Level level;
+
+    public string name;
+    public Sprite Picture;
+    public int maxScore;
+
+    public Text nameText;
+    public Image image;
+	}
+}
+
+[System.Serializable]
+public struct Level
+{
+    public string name;
+    public int maxScore;
+    public bool isComplete;
+    public bool isUnlocked;
+}
