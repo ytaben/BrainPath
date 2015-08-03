0a3a65a67fd37295068b18b2415a2f6e421f605a
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-03 18:36:37 -0400

    Add the forgotten labelUpdater file

---
+++ BrainPath/Assets/Scripts/LabelUpdater.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LabelUpdater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void updateLabels()
+    {
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("BrainPart"))
+        {
+            BrainNode3D brainNode = gameObject.GetComponent<BrainNode3D>();
+            if (brainNode) { brainNode.UpdateLabel(); }
+        }
+    }
+}
