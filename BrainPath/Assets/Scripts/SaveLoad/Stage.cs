251541721ca5d78ed304c7c957b6ba9bffbb0651
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-03 17:14:42 -0400

    Add StageManager class

---
+++ BrainPath/Assets/Scripts/SaveLoad/Stage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour {
+
+    public string name;
+    public Sprite pircure; //Picture to display 
+    public LevelManager[] levels; //Levels that belong to this stage
+
+    public Text nameText;
+    public Image imageObject;
+}
