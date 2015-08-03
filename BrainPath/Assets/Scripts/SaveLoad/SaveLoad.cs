4d0e253a3ec4bbfe5700b8a2ad6d3a05bd5bf097
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-03 18:37:34 -0400

    Add SaveLoad script for saving and loading level info
    Already implemented save and load levels

---
+++ BrainPath/Assets/Scripts/SaveLoad/SaveLoad.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+public static class SaveLoad  {
+    //Use list for serializing stuff. Use dictionary for runtime access
+    public static List<Level> levels;
+    public static Dictionary<string, Level> levelsDict;
+
+    private const string savePath = "/GameStage.save";
+
+    public static void Initialize()
+    {
+        levelsDict = new Dictionary<string, Level>();
+
+        LoadLevels();
+    }
+    public static void SaveLevels()
+    {
+        levels.Clear();
+        foreach (Level level in levelsDict.Values)
+        {
+            levels.Add(level);
+        }
+        
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + savePath);
+        bf.Serialize(file, levels);
+        file.Close();
+    }
+
+    public static void LoadLevels()
+    {
+        if (File.Exists(Application.persistentDataPath + savePath))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            SaveLoad.levels = (List<Level>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+	
+}
