1eec287fcf9882d1c678efc33e119b5987adb63c
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-06 12:37:24 -0400

    Add update view to StageManager
    This method will be used to update and display the info on single stage views

--- BrainPath/Assets/Scripts/SaveLoad/StageManager.cs
+++ BrainPath/Assets/Scripts/SaveLoad/StageManager.cs
@@ -11,5 +11,23 @@ public class StageManager : MonoBehaviour {
     public Text levelsCompleteText;
     public Text totalScoreText;
 
+    public void UpdateView()
+    {
+        int levelsComplete = 0;
+        int totalScore = 0;
+
+        foreach (LevelManager manager in levels)
+        {
+            Level level = SaveLoad.levelsDict[manager.name];
+
+            if (level.isComplete) levelsComplete += 1;
+
+            if (level.maxScore > 0) totalScore += level.maxScore;
+        }
+
+        levelsCompleteText.text = "Levels Completed: " + levelsComplete.ToString() + "/" + levels.Length.ToString();
+        totalScoreText.text = "Total Score: " + totalScore.ToString();
+    }
+
 
 }
