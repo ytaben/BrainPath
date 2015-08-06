9a93b95b2dd74e1e7ac56436961a26da213dd3a0
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-06 12:24:15 -0400

    Remove image reference from stage manager as it is no longer used

--- BrainPath/Assets/Scripts/SaveLoad/Stage.cs
+++ BrainPath/Assets/Scripts/SaveLoad/Stage.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 public class StageManager : MonoBehaviour {
 
     public string name;
-    public Sprite pircure; //Picture to display 
     public LevelManager[] levels; //Levels that belong to this stage
 
     public Text nameText;
-    public Image imageObject;
+
 }
