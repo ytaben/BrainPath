d8a934f41b0ee46ccd614f8c69cdf9b1349b4f59
Yaroslav Taben <yaroslav.taben@mail.utoronto.ca>
2015-08-10 11:08:25 -0400

    Add button component to level manager

--- BrainPath/Assets/Scripts/SaveLoad/Level.cs
+++ BrainPath/Assets/Scripts/SaveLoad/Level.cs
@@ -3,16 +3,26 @@ using System.Collections;
 using System.IO;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Button))]
 public class LevelManager : MonoBehaviour {
     Level level;
 
     public string name;
+    public string sceneName;
     public Sprite Picture;
     public int maxScore;
 
     public Text nameText;
     public Image image;
+
+    void Start()
+    { GetComponent<Button>().onClick.AddListener(OnClick); }
 	
+    void OnClick()
+    {
+        SaveLoad.currentLevel = name;
+        Application.LoadLevel(sceneName);
+    }
 }
 
 [System.Serializable]
